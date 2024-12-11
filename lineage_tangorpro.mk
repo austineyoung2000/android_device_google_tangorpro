b5a04156de79b27bda713f234edf936feb1f68c4
Michael Bestas <mkbestas@lineageos.org>
2024-12-11 04:12:23 +0200

    Update fingerprint/build description from AP4A.241205.013
    Change-Id: Ia86f61708aca23842e73e3e1870da21e81d1cdc8

--- lineage_tangorpro.mk
+++ lineage_tangorpro.mk
@@ -26,8 +26,8 @@ TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 2560
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="tangorpro-user 15 AP3A.241105.007 12470370 release-keys" \
-    BuildFingerprint=google/tangorpro/tangorpro:15/AP3A.241105.007/12470370:user/release-keys \
+    BuildDesc="tangorpro-user 15 AP4A.241205.013 12621605 release-keys" \
+    BuildFingerprint=google/tangorpro/tangorpro:15/AP4A.241205.013/12621605:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
