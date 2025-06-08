1d0346ba2946da55b971daacffe18ec7f93fb473
Austin Young <austineyoung2000@gmail.com>
2025-06-08 18:02:47 -0400

    tangorpro: Define axion flags

--- lineage_tangorpro.mk
+++ lineage_tangorpro.mk
@@ -25,6 +25,10 @@ PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 2560
 
+AXION_CAMERA_REAR_INFO := 8
+AXION_CAMERA_FRONT_INFO := 8
+AXION_MAINTAINER := EliteDarkKaiser
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="tangorpro-user 16 BP2A.250805.005 13691446 release-keys" \
     BuildFingerprint=google/tangorpro/tangorpro:16/BP2A.250805.005/13691446:user/release-keys \
