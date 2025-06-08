08b94677d68736e8aad7864b7bc4d7e84335cb5a
Austin Young <austineyoung2000@gmail.com>
2025-06-08 18:05:26 -0400

    tangorpro: Ship viperfx

--- lineage_tangorpro.mk
+++ lineage_tangorpro.mk
@@ -26,6 +26,8 @@ AXION_CAMERA_REAR_INFO := 8
 AXION_CAMERA_FRONT_INFO := 8
 AXION_MAINTAINER := EliteDarkKaiser
 
+TARGET_INCLUDE_VIPERFX := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="tangorpro-user 16 BP4A.260205.001 14624666 release-keys" \
     BuildFingerprint=google/tangorpro/tangorpro:16/BP4A.260205.001/14624666:user/release-keys \
