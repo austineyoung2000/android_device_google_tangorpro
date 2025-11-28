9c6aa9fcd46e355c194ebf14878a78adc4b7ef76
Michael Bestas <mkbestas@lineageos.org>
2025-11-28 20:28:54 +0200

    tangorpro: Relocate even more things to vendor
    Change-Id: Ic7872d3a7cbac2f0c2e01162697567972027389a

--- device-tangorpro.mk
+++ device-tangorpro.mk
@@ -40,13 +40,6 @@ DISABLE_TELEPHONY_EUICC := true
 
 include device/google/gs201/device-shipping-common.mk
 
-# Touch
-PRODUCT_PACKAGES += \
-    dump_touch.sh \
-    predump_gti0.sh \
-    predump_touch.sh \
-    touch_gti_ical
-
 # UWB
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.uwb.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.uwb.xml
@@ -54,9 +47,6 @@ PRODUCT_COPY_FILES += \
 # Recovery files
 PRODUCT_COPY_FILES += \
     device/google/tangorpro/recovery/init.recovery.device.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.tangorpro.rc
-
-# PowerStats HAL
-PRODUCT_SOONG_NAMESPACES += device/google/tangorpro/powerstats
 
 # Bluetooth HAL and Pixel extension
 include device/google/tangorpro/bluetooth/syna_default.mk
@@ -178,10 +168,6 @@ PRODUCT_PACKAGES += \
     libEGL_angle \
     libGLESv1_CM_angle \
     libGLESv2_angle
-
-# Dumpstate
-PRODUCT_PACKAGES += \
-    dump_gsc.sh
 
 # GMS
 WITH_GMS_COMMS_SUITE := false
