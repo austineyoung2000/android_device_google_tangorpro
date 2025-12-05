ac0c6d0686b9ed0e789f74ff18e6f25799de14cb
Michael Bestas <mkbestas@lineageos.org>
2025-12-05 11:19:34 +0200

    tangorpro: Replace userdebug checks in scripts
    Change-Id: Ia326a3eff1fad5badf1282850939a90663b6d1c4

--- tangorpro/extract-files.py
+++ tangorpro/extract-files.py
@@ -50,6 +50,8 @@ blob_fixups: blob_fixups_user_type = {
         .patch_file('osaifu-keitai.patch'),
     'vendor/etc/init/init.modem_logging_control.rc': blob_fixup()
         .regex_replace(' && property:ro.debuggable=0', ''),
+    'vendor/etc/init/init.storage.rc': blob_fixup()
+        .regex_replace('ro.build.type=userdebug', 'ro.debuggable=1'),
     ('vendor/lib/libspeechenhancer.so', 'vendor/lib64/libspeechenhancer.so'): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
