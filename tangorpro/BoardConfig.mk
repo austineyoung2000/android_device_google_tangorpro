1a157947499e453d78d57389055734be9602bf67
Michael Bestas <mkbestas@lineageos.org>
2025-12-06 05:57:43 +0200

    tangorpro: Shuffle BoardConfig
    Change-Id: I920841e4fbd3912cc178912300b81fe86c3147cc

--- tangorpro/BoardConfig.mk
+++ tangorpro/BoardConfig.mk
@@ -5,27 +5,30 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-TARGET_BOARD_INFO_FILE := device/google/tangorpro/board-info.txt
-TARGET_BOOTLOADER_BOARD_NAME := tangorpro
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_BOOTLOADER_BOARD_NAME := $(DEVICE_CODENAME)
 TARGET_SCREEN_DENSITY := 320
 
-# Enable load module in parallel
+include device/google/gs201/BoardConfig-common.mk
+
+# Kernel
+BOARD_KERNEL_CMDLINE += swiotlb=noforce
+
+# Kernel modules
 BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
 
-# The modules which need to be loaded in sequential
 BOARD_KERNEL_CMDLINE += fips140.load_sequential=1
 BOARD_KERNEL_CMDLINE += exynos_drm.load_sequential=1
 
-BOARD_KERNEL_CMDLINE += swiotlb=noforce
-
-include device/google/gs201/BoardConfig-common.mk
-include device/google/tangorpro/sepolicy/tangorpro-sepolicy.mk
-include device/google/tangorpro/wifi/BoardConfig-wifi.mk
-
-# Kernel modules
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/recovery/modules.blocklist.vendor_kernel_boot
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/recovery/modules.load.vendor_kernel_boot))
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)))
+
+# SEPolicy
+include device/google/tangorpro/sepolicy/tangorpro-sepolicy.mk
+
+# WiFi
+include $(DEVICE_PATH)/wifi/BoardConfig-wifi.mk
 
 include $(VENDOR_PATH)/BoardConfigVendor.mk
